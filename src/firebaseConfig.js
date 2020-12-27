8e62abd49d8d9009a759b9370ccc4e885b7ce849
Afzal-BiDhan <67520554+Afzal-BiDhan@users.noreply.github.com>
2020-12-27 17:50:05 +0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB0BqwWAGw3HLlkaLR5WEAomstTLm1nOJM",
-  authDomain: "afzal-bidhan-d.firebaseapp.com",
-  databaseURL: "https://afzal-bidhan-d.firebaseio.com",
-  projectId: "afzal-bidhan-d",
-  storageBucket: "afzal-bidhan-d.appspot.com",
-  messagingSenderId: "817755712844",
-  appId: "1:817755712844:web:a01fdcc01eb31797cbf3e9"
+  enter your firebase priject config info
 };
 
 export default firebaseConfig
